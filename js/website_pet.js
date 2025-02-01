ec29b925f09be38e229e28ef6e4399338ba2fa55
Cuppixx <116835172+Cuppixx@users.noreply.github.com>
2025-02-02 00:18:33 +0100

    Update website_pet.js

--- js/website_pet.js
+++ js/website_pet.js
@@ -2,13 +2,10 @@ document.addEventListener("mousemove", (event) => {
     const pet = document.getElementById("pet");
 
     const petWidth = pet.offsetWidth;
-    const petHeight = pet.offsetHeight;
 
     // Get the mouse position
     const mouseX = event.clientX;
-    const mouseY = event.clientY;
 
     // Set pet position based on mouse coordinates
     pet.style.left = `${mouseX - petWidth / 2}px`;  // Center the pet on the cursor horizontally
-    pet.style.top = `${mouseY - petHeight / 2}px`;  // Center the pet on the cursor vertically
 });

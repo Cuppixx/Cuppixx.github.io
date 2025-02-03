f859fad2afa0376ad11c767319b0152ecbd276e5
Cuppixx <116835172+Cuppixx@users.noreply.github.com>
2025-02-03 22:39:29 +0100

    Update position_socials.js

--- js/position_socials.js
+++ js/position_socials.js
@@ -4,13 +4,10 @@ function updateDynamicPositions() {
   const main = document.querySelector('main');
 
   const navHeight = nav.offsetHeight;
-
   const mainWidth = main.offsetWidth;
-  const mainMarginTop = parseFloat((window.getComputedStyle(main)).marginTop) || 15;
 
   document.documentElement.style.setProperty('--nav-height', navHeight + 'px');
   document.documentElement.style.setProperty('--main-width', mainWidth + 'px');
-  document.documentElement.style.setProperty('--main-margin', mainMarginTop + 'px');
 }
 
 window.addEventListener('load', updateDynamicPositions);

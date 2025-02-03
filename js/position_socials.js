a9281c320ae2941727b6b1d724f19ea8d2af4fb6
Cuppixx <116835172+Cuppixx@users.noreply.github.com>
2025-02-03 23:18:18 +0100

    Update position_socials.js

--- js/position_socials.js
+++ js/position_socials.js
@@ -1,14 +1,21 @@
-function updateDynamicPositions() {
-
+const updateDynamicPositions = () => {
   const nav = document.querySelector('nav');
   const main = document.querySelector('main');
+  const socials = document.querySelector('.socials');
 
   const navHeight = nav.offsetHeight;
   const mainWidth = main.offsetWidth;
+  const mainStyles = window.getComputedStyle(main);
+  const mainMarginTop = parseFloat(mainStyles.marginTop) || 15;
+  const mainPaddingTop = parseFloat(mainStyles.paddingTop) || 15;
+  const socialsWidth = socials.offsetWidth;
 
-  document.documentElement.style.setProperty('--nav-height', navHeight + 'px');
-  document.documentElement.style.setProperty('--main-width', mainWidth + 'px');
-}
+  document.documentElement.style.setProperty('--nav-height', `${navHeight}px`);
+  document.documentElement.style.setProperty('--main-width', `${mainWidth}px`);
+  document.documentElement.style.setProperty('--main-margin-top', `${mainMarginTop}px`);
+  document.documentElement.style.setProperty('--main-padding', `${mainPaddingTop}px`);
+  document.documentElement.style.setProperty('--socials-width', `${socialsWidth}px`);
+};
 
 window.addEventListener('load', updateDynamicPositions);
 window.addEventListener('resize', updateDynamicPositions);

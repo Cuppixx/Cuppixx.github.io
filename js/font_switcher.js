82a7af005a29ae4a45e22f7e5fda96c7161c7757
Cuppixx <CuppiXD@gmx.de>
2025-02-21 22:14:08 +0100

    font switcher button click color and added font switching

--- js/font_switcher.js
+++ js/font_switcher.js
@@ -1,16 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.font-switcher');
     const paragraph = button.parentElement.querySelector('p');
+
+    const computedStyle = getComputedStyle(paragraph);
+    const defaultFont = computedStyle.fontFamily;
+    const defaultFontSize = computedStyle.fontSize;
     
-    const defaultFont = getComputedStyle(paragraph).fontFamily;
     let toggled = false;
 
     button.addEventListener('click', function() {
         const newFont = button.getAttribute('alt-font');
+        const newFontSize = button.getAttribute('alt-font-size');
+        
         if (!toggled) {
             paragraph.style.fontFamily = newFont;
+            if (newFontSize) {
+                paragraph.style.fontSize = newFontSize;
+            }
         } else {
             paragraph.style.fontFamily = defaultFont;
+            paragraph.style.fontSize = defaultFontSize;
         }
         toggled = !toggled;
     });
